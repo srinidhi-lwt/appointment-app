9582b27e5a5eb5a27f8ad26b15c92719506ecf22
Srinidhi G S <srini.satti+lwt@gmail.com>
2019-09-14 16:47:38 +0530

    adding scope and before save for dayname

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,10 @@
 class Event < ApplicationRecord
+
+  before_save :set_day_of_week
+
+  scope :by_kind, -> (type) { where(kind: type) }
+  scope :recurring, -> { where(weekly_recurring: true) }
+
+  def set_day_of_week
+  end
 end
