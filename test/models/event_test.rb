2192b4c1cdefbc8e6ed89a83b6812c90e31dfb88
Srinidhi G S <srini.satti+lwt@gmail.com>
2019-09-14 16:15:30 +0530

    added simple test case

--- test/models/event_test.rb
+++ test/models/event_test.rb
@@ -1,7 +1,20 @@
 require 'test_helper'
 
 class EventTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+
+  test "one simple test example" do
+
+    Event.create kind: 'opening', starts_at: DateTime.parse("2014-08-04 09:30"), ends_at: DateTime.parse("2014-08-04 12:30"), weekly_recurring: true
+    Event.create kind: 'appointment', starts_at: DateTime.parse("2014-08-11 10:30"), ends_at: DateTime.parse("2014-08-11 11:30")
+
+    availabilities = Event.availabilities DateTime.parse("2014-08-10")
+    assert_equal '2014/08/10', availabilities[0][:date]
+    assert_equal [], availabilities[0][:slots]
+    assert_equal '2014/08/11', availabilities[1][:date]
+    assert_equal ["9:30", "10:00", "11:30", "12:00"], availabilities[1][:slots]
+    assert_equal [], availabilities[2][:slots]
+    assert_equal '2014/08/16', availabilities[6][:date]
+    assert_equal 7, availabilities.length
+  end
+
 end
