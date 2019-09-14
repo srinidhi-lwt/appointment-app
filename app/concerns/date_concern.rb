a4d5325ab8ba4fcee2cec5ec524cbd64ba2185b8
Srinidhi G S <srini.satti+lwt@gmail.com>
2019-09-14 17:15:11 +0530

    getting appointments for next 7 days and openings for latest day of week

--- app/concerns/date_concern.rb
+++ app/concerns/date_concern.rb
@@ -5,5 +5,13 @@ module DateConcern
     def self.day_name_for(date = Date.today)
       date.strftime("%A").downcase
     end
+
+    def self.seven_days_from(date)
+      [date, date + 1.week]
+    end
+
+    def self.seven_days_range(date)
+      date.beginning_of_day...(date + 1.week).end_of_day
+    end
   end
 end
--- app/models/event.rb
+++ app/models/event.rb
@@ -4,9 +4,27 @@ class Event < ApplicationRecord
 
   scope :by_kind, -> (type) { where(kind: type) }
   scope :recurring, -> { where(weekly_recurring: true) }
+  scope :by_date, -> (dates) { where('date(starts_at) IN (?)', dates) }
 
   def set_day_of_week
     self.day_of_week = day_name_for(starts_at.to_date)
+  end
+
+  def self.availabilities(given_date)
+    given_date, upto_seven_days = seven_days_from(given_date)
+    next_seven_days = seven_days_range(given_date)
+
+    appointments = by_kind('appointment')
+                     .where(starts_at: next_seven_days)
+                     .group_by(&:day_of_week)
+
+    latest_opening_dates = by_kind('opening').recurring
+                            .select('MAX(date(starts_at)) AS event_date')
+                            .group('day_of_week')
+                            .map(&:event_date)
+
+    openings = by_date(latest_opening_dates)
+                .group_by(&:day_of_week)
   end
 
   private
